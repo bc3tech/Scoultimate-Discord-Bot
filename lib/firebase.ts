0014d932a43cd9ace806b862a981a03e393c3785
Brandon🌩️H <bc3tech@users.noreply.github.com>
2025-01-10 10:00:03 -0800

    remove firebase usage

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,7 @@ import { App, cert } from "firebase-admin/app";
 import * as admin from "firebase-admin";
 import { getFirestore, FieldValue } from "firebase-admin/firestore";
 
-const app: App = admin.initializeApp({
-  credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  }),
-});
+const app: App = admin.initializeApp();
 
 export const db = getFirestore(app);
 
